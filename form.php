1668261a3b35720c61f607e85ea28bb5d8cdcfdb
Becode <benny.ho2530@gmail.com>
2019-08-05 16:49:17 +0200

    modified codes

--- form.php
+++ form.php
@@ -12,56 +12,110 @@ $name = $email = $message = "";
 // Sanitization
 
 $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+<?php
+
+
+// In PHP, the command
+
+echo "<pre>";
+print_r($_POST);
+
+$nameErr = $emailErr = $messageErr =  $addressErr = "";
+
+$name = $email = $message = $address = "";
+
+
+// 1.  Sanitization
+
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
 echo $email;
 
-// Validation
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// 2.  Validation
 
 // define variables and initialize with empty values
 
-
-if (isset($_POST['name'])) {
-
-    $nameErr = 'Name is required';
-} else {
-    echo 'this is valid';
-}
-
-
-if (empty($_POST['email'])) {
-    echo 'You forgot to enter your email';
-} else {
-    echo 'this is valid';
-}
-
-if (empty($_POST['yourMessage'])) {
-    echo 'You forgot to write your message';
-} else {
-    echo 'this is valid';
-}
-/*
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-        $nameErr = "Missing";
+    if (empty($_POST['name'])) {
+        $nameErr = 'Name is required';
     } else {
-        $name = $_POST["name"];
-    } 
-
+        $name = test_input($_POST['Name']);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z]*$/", $name)) {
+            $nameErr = "Only letters and white space allowed";
+        }
+    }
+    
     if (empty($_POST['email'])) {
-        $emailErr = "Missing";
+        $emailErr = 'Email is required'; 
     } else {
-        $email = $_POST['email'];
+        $email = test_input($_POST['email']);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+        }
     }
-
-    if (empty($_POST["yourMessage"])) {
-        $messageErr = "Missing";
+    
+    if (empty($_POST['address'])) {
+         $addressErr = 'Address is required'; 
     } else {
-        $message = $_POST['yourMessage'];
+         $address = test_input($_POST['address']);
     }
-       
+    
+    if (empty($_POST['yourMessage'])) {
+        $messageErr = 'Message is required'; 
+    } else {
+        $message = test_input($_POST['message']);
+    }
+    
+    
 }
 
+// 3. EXECUTION
+
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+  //  $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();  = smtp.gmail.com                          // Set mailer to use SMTP
+ //$mail->Host       = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'benny.ho2530@gmail.com';                     // SMTP username
+    $mail->Password   = 'ujedyuruopujhtgi';                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 */
 ?>
+
 
--- index.php
+++ index.php
@@ -1,33 +1,47 @@
+<?php include('form.php'); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Form</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles2.css">
 </head>
 <body>
-        
-    <form class="form" action="form.php" method="POST">
-        <div class="form-left">
-            <div class="form-q">
-                <label for="name" id="nameTag">Fullname</label>
-                <span class="error"><?= $nameErr ?></span>
-                <label for="email" id="emailTag">Email</label>
-                <label for="yourMessage" id="messageTag">Your Message</label>
-            </div>
-        </div>
-        <div class="form-right">
-            <div class="form-a">
-                <input type="text" name="name" id="fullName">
-                <input type="text" name="email" id="email">
-                <textarea  id="" cols="30" rows="10" name="yourMessage" id="yourMessage"></textarea>
-            </div>
-        </div>
-        <input class="sendBtn" type="submit" value="Submit" name="submit">
-    </form>
 
-    
+<div class="wrapper">
+  <section class="header">
+    <h1>Contact Us</h1>  
+  </section>  
+  <main>
+    <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
+      <div class="input-group">
+        <label for="name" id="nameTag">Name</label>
+        <span class="error"><?= $nameErr ?></span> 
+        <input type="text" id='name' name='name' tab-index='1'value='<?=$name?>'/>
+      </div>
+      <div class="input-group">
+        <label for="email" id="emailTag">Email</label>
+        <span class="error"><?= $emailErr ?></span> 
+        <input type="text" id='email' name='email' tab-index='2' value='<?=$email?>'/>
+      </div>
+      <div class="input-group">
+        <label for="address" id="addressTag">Address</label>
+       <span class="error"><?= $addressErr ?></span> 
+        <input type="text" id='address' name='address' tab-index='3' value='<?=$address?>'/>
+      </div>
+      <div class="input-group">
+        <label for="message" id="messageTag">Message</label>
+        <span class="error"><?= $messageErr ?></span>
+        <textarea id='message' name='message' tab-index='4'></textarea>
+      </div>
+      <div class="input-group">
+        <input type="submit" value="submit" tab-index='5'/>
+      </div>
+    </form>
+  </main>
+</div>
 </body>
 </html>
